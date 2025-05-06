424fa4df24de377fdb81839ad45dbe3a07e96401
PWD <powderserialgamer07@gmail.com>
2025-05-07 00:06:06 +0100

    Implemented a bunch of stuff
    - DB logic and mod loader insertion implemented
    - Mod insertion partially implemented
    - Bunch of extra comments explaining stuff and complaining
    - Logic for getting all mods implemented

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ import json
 import sqlite3
 '''
 Putting this here so I don't forget:
-most mainstream mods are on multiple versions and modloaders. For this, we'll look through its files and get the downloads for each versions and loader, then
-we'll make duplicate entries in the db (one for each version and modloader supported) and add the downloads for each of them
+Instead it'll be one databse with one table containing each mods and their downloads for each version (and a total column), then another table which is just 
+a mod loader per line with a single columns containing their downloads
+
+Curseforge's fuck ass api only offers per mod statistics so I'm going to have to make a lot of request
+Let's hope I don't instantly get rate limited or have my api key revoked
 '''
 
+# API setup
 load_dotenv()
 API_URL = "https://api.curseforge.com/"
 API_KEY = os.getenv('key')
@@ -19,6 +23,80 @@ headers: dict = {
 }
 
 
+# To escape column names since for sqlite3 you can't have column names be stuff like 1.21.2
+def escape_column_name(name: str) -> str:
+    return f'"{name}"'
+
+
+# Self explanatory, just creating the db (if it doesn't exist)
+def create_db(db_path: str = 'mods.sqlite3', version_list: list = []):
+    # DB Setup
+    conn = sqlite3.connect(db_path)
+    cur = conn.cursor()
+
+    # Escaping Column Names
+    data_type: str = "INTEGER"
+    columns: list = []
+    for i in version_list:
+        column = f'{escape_column_name(i)} {data_type}'
+        columns.append(column)
+    # No need to put this one through escape_column_name I think
+    columns.append('Total INTEGER')
+
+    # Create DB Tables
+    cur.executescript('''
+    CREATE TABLE IF NOT EXISTS Mods (
+        mod_id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL,
+        {', '.join(columns)}
+    );
+    CREATE TABLE IF NOT EXISTS Mod_Loaders (
+        name TEXT PRIMARY KEY,
+        downloads INTEGER
+    )
+    ''')  # Should create a column for each versions available in the mod table, also we're not getting per version downloads for the mod loaders
+    conn.commit()
+    return conn
+
+
+def insert_mod(conn, mod_data: dict, version_list: list):
+    """
+    mod_data should be a a dictionary, something like this (all mods passed with mod_data should have at least 1K downloads):
+    {
+        'mod_id': 0,
+        'name': 'example_mod',
+        '1.21.2': 1234,
+        '1.20.1': 4590
+        'total': 5824 # Optional, maybe we calculate it here? or not the API sends the total I think
+    }
+    mod loaders will be handled in another function.
+
+    mod list is passed to compare the versions in mod_data with it, so that if the mod doesn't have a version in version_list, we add it in the dictionnary with 0 downloads
+    """
+    cur = conn.cursor()
+    mod_id = mod_data['mod_id']
+    name = mod_data['name']
+    for i in version_list:
+        if i not in mod_data:
+            # If the mod doesn't have a version in version_list, we add it in the dictionary with 0 downloads
+            mod_data[i] = 0
+    # Now, how will I add those values to the database? I have no idea so TODO: Find a way to add them to the database
+
+    conn.commit()
+
+
+# Pretty simple, actually getting the mod_loader_data will be the hard part
+def insert_mod_loader(conn, mod_loader_data: dict):
+    cur = conn.cursor()
+    name = mod_loader_data['name']
+    downloads = mod_loader_data['downloads']
+    cur.execute('''
+    INSERT OR IGNORE INTO Mod_Loaders (name, downloads) VALUES (?, ?)
+    ''', (name, downloads))
+    conn.commit()
+
+
+# Self explanatory, just getting the list of versions
 def get_version_list() -> list:
     versions_raw = requests.get(
         API_URL + "v1/minecraft/version", headers=headers)
@@ -28,11 +106,13 @@ def get_version_list() -> list:
     return versions
 
 
+# Same as above except we're getting the list of mod loaders
 def get_mod_loaders_list() -> list:
     mod_loaders_raw = requests.get(
         API_URL + "v1/mods/loaders", headers=headers)
     mod_loaders_raw = mod_loaders_raw.json()
-    # Now it uses ids instead of strings whyyyyy, it even returns the name as a string(which will be useful for displaying it to the user)
+    # Now it uses ids instead of strings whyyyyy, it even returns the name as a strings so why offer both
+    # I kinda dislike these one liners so I might change it later
     mod_loaders: list = [item['type'] for item in mod_loaders_raw['data']]
     return mod_loaders
 
@@ -40,3 +120,18 @@ def get_mod_loaders_list() -> list:
 def main():
     versions = get_version_list()
     mod_loaders = get_mod_loaders_list()
+    con = create_db(version_list=versions)
+    for version in versions:
+        for mod_loader in mod_loaders:
+            index = 0
+            while True:
+                # Here's to many more api requests
+                mods_raw = requests.get(API_URL + f"v1/mods/search", params={
+                    'gameVersion': version,
+                    'modLoaderType': mod_loader,
+                    'index': index
+                }, headers=headers)
+                mods_raw = mods_raw.json()
+                index += 50
+                if mods_raw["pagination"]["resultCount"] == 0:
+                    break
